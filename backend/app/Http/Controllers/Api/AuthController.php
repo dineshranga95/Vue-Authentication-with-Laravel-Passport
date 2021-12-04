4ffa769bad3a9d994643d3d180dadd3c9bd3fcf8
dineshranga95 <dineshranga570@gmail.com>
2021-12-04 17:27:25 +0530

    login api created

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -6,22 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use App\User;
 use Exception;
 
 class AuthController extends Controller
-{
+{    
     public function register(Request $request){
-        return 'register';
-    }
-    public function login(Request $request){
         $validate= Validator::make($request->all(),[
             'name'=> 'required|min:3',
             'email'=>'required|email|unique:users',
             'password'=>'required|min:6'
         ]);
         if ($validate->fails()){
-            return response()->json(['msg'=>'not a user', 'data'=>$validate->errors()],422);
+              return response()->json(['msg'=>'Validation error', 'data'=>$validate->errors()],422);
         };
         try {
             $user = User::create([
@@ -29,14 +27,33 @@ class AuthController extends Controller
                 'email' =>$request->email,
                 'password' =>Hash::Make($request->password)  
             ]);
-            return response()->json(['status' =>true, 'msg'=>'user registration is success' ,'user' => $user]);
+            return response()->json(['status' =>true, 'msg'=>'user registration is success' ,'name' => $user->name]);
         } catch(Exception $e){
             return response()->json([
                 'msg'=>$e->getMessage()
             ], $e->getCode());
-        }
             
-           
+        } 
         
+    }
+    public function login(Request $request){
+        $validate= Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+        if ($validate->fails()){
+              return response()->json(['msg'=>'Validation error', 'data'=>$validate->errors()],422);
+        };
+        
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            $data['name'] = $user->name;
+            $data['access_token'] = $user->createToken('accessToken')->accessToken;
+
+            return response()->json(['msg'=>'succesfully logged in', 'user'=>$data]);
+        }else{
+            return response()->json(['msg'=>'unauthorized'], 401);
+        }
     }
 }
