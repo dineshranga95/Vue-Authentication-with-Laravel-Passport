c241f45edb8dc1f07d94cf27952bb302ff6b7826
dineshranga95 <dineshranga570@gmail.com>
2021-12-06 17:59:04 +0530

    login part created in vue

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null || localStorage.getItem('accessToken')
   },
   mutations: {
+    setToken( state, token){
+        state.token= token
+    }
+  },
+  getters:{
+    
   },
   actions: {
+    login(context, credential){
+      axios.post('http://127.0.0.1:8000/api/auth/login', credential)
+      .then(response=>{
+        localStorage.setItem('accessToken',response.data.user.access_token)
+
+        context.commit('setToken', response.data.user.access_token)
+      }).catch(error=>{
+        console.warn(error)
+      })
+      
+    }    
   },
   modules: {
-  }
+  } 
 })
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="container py-4">
-    <h1>This is an login page</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+        <div class="card">
+          <div class="card-header">User Login Form</div>
+          <div class="card-body">
+            <form @submit.prevent="login">
+                <div class="form-group mb-3">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" v-model="credential.email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group mb-3">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" v-model="credential.password" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="text-end">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>                
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>    
   </div>
 </template>
 
 <script>
 export default {
-    
+  name:"Login",
+    data(){
+      return{
+        credential:{
+          email:null,
+          password:null
+        }
+      }
+    },
+    methods:{
+      login(){
+        this.$store.dispatch('login', this.credential)
+      }
+    }
+
 }
 </script>
